d03718b6c08640f459d8b1d27e718c9acce6e27b
JesusPerezPaco <108727858+JesusPerezPaco@users.noreply.github.com>
2023-09-04 19:23:41 -0400

    Add files via upload

---
+++ indicadores.py
@@ -0,0 +1,43 @@
+import wbgapi as wb   # I use wb as a namespace in all my work
+import pandas as pd   # optional but highly recommended for wbgapi
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+pais_input = input("Pais a buscar: ")
+pais = wb.economy.info(q=pais_input)
+
+print(pais)
+
+id_input = input("Seleccione el id: ")
+
+
+
+data_list = []
+choice = 0
+
+tema_input = str(input("Variable a buscar: "))
+tema = wb.series.list(q=tema_input)
+
+for id in tema:
+    id_title = id["id"]
+    data_list.append(id_title)
+    print(str(choice) + ": " + str(id["value"]))
+    choice = choice + 1
+
+selection = int(input("Seleccione el indicador deseado: "))
+
+año_inicio = int(input("Seleccione el año de inicio: "))
+año_final = int(input("Seleccione el año final: "))
+
+data = wb.data.DataFrame(data_list[selection], id_input, range(año_inicio, año_final), index="time",numericTimeKeys=True, 
+                         labels=True).plot(figsize=(10, 6))
+
+plt.title(str(id["value"]))
+
+plt.show()
+
+
+
+
+
